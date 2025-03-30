0550a5c6889a246d3662458c08482385c88fa628
Jordan <jordanjordaniza@gmail.com>
2025-03-30 01:37:57 -0500

    update page

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Inicializa Swiper
 var swiper = new Swiper('.swiper-container', {
-    loop: true, // Habilita el loop
-    speed: 600, // Velocidad de transición
-    autoplay: {
-        delay: 2500, // Tiempo entre cada slide
-        disableOnInteraction: false, // Mantener el autoplay al interactuar
-    },
-    navigation: {
+    loop: true, // Habilita el ciclo infinito
+    navigation: { // Agrega navegación con botones
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    pagination: { // Paginación de los slides (si la quieres agregar)
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: { // Auto Play para deslizar las imágenes automáticamente
+        delay: 3000, // Desliza cada 3 segundos
     },
 });
