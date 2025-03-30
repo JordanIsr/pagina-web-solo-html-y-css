cec5c7723235f49bd1b4711d880302c3c46e7160
Jordan <jordanjordaniza@gmail.com>
2025-03-30 03:46:26 -0500

    carrusel modificado

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
-    loop: true,
+    loop: false, // Asegura que el carrusel funcione en bucle infinito
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
+        clickable: true, // Permite que los puntos de paginación sean clicables
     },
     autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+        delay: 3000, // Cambia de slide automáticamente cada 3 segundos
+        disableOnInteraction: false, // Continúa el autoplay incluso después de interacción
     },
-    speed: 500,
+    speed: 500, // Velocidad de transición entre slides
 });
